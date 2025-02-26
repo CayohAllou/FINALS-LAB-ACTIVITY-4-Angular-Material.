8eac143605eda07f267f7124cdf672de30eabdf0
CayohAllou <cayohallouu@gmail.com>
2025-02-26 10:46:55 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./register/register.component";
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { StepperComponent } from './stepper/stepper.component';
+import { CardComponent } from './card/card.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule,RegisterComponent],
+  imports: [RouterOutlet, ReactiveFormsModule,RegisterComponent, StepperComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
