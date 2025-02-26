52d7c6b7dfb8281f08b5a6dcab6c447fd7914602
CayohAllou <cayohallouu@gmail.com>
2025-02-26 10:45:27 +0800

    Add files via upload

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-card',
+  imports: [MatCardModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
